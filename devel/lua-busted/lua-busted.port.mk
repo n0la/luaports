5ac1d42779eb3601e06e498baa887d9476686a6e
Florian Stinglmayr <florian@n0la.org>
2016-04-04 21:03:28 +0200

    Add module for lua-busted

---
+++ devel/lua-busted/lua-busted.port.mk
@@ -0,0 +1,18 @@
+# $OpenBSD: lua-busted.port.mk
+
+CATEGORIES +=		lang/lua
+
+MODBUSTED_BIN =		${PREFIX}/bin/${MODLUA_DEP}-busted
+
+#
+# Path were the unit tests are. A common standard appears to be
+# "spec"
+#
+MODBUSTED_SPECDIR ?=	spec
+
+.if ${NO_TEST:L} == "no"
+TEST_DEPENDS +=		devel/lua-busted
+
+do-test:
+	cd ${WRKSRC} && ${MODBUSTED_BIN} ${MODBUSTED_SPECDIR}
+.endif
